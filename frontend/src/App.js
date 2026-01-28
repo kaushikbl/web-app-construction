7a8ab07009f5e40de7186db817530da6757a087c
kaushikbl <kaushikblokesh@gmail.com>
2026-01-28 16:16:44 +0530

    typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -102,7 +102,7 @@ function App() {
 
           {Object.entries(categories).map(([group, items]) => (
             <optgroup key={group} label={group}>
-              {items.map((cat) => (
+              {categories.map((cat) => (
               <option key={cat._id} value={cat.name}>
               {cat.name}
               </option>
